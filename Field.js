78695f41adab83587b2016b7a371a5b9fdcace03
akhokhlov <akhokhlov@che-d-041.rocketsoftware.com>
2012-04-02 19:13:34 +0600

    init

---
+++ Field.js
@@ -0,0 +1,7 @@
+function Field(x, y, width, height){
+	this.x = x;
+	this.y = y;
+	this.width = width;
+	this.height = height;
+	
+}
--- environment.js
+++ environment.js
@@ -11,13 +11,20 @@ var theCreature2;
 var interval = 100;
 var creatureList = new Array();
 var grid = new MultiDimensionalArray(maxWidth, maxHeight);
+var fields = new Array();
+
+
 
 function rand(maxNum) {
 	return Math.round(maxNum * Math.random());
 }
 
 function createNewEnvironment() {
-
+	fields.push(new Field(0, 0, 77, 77));
+	
+	
+	
+	
 	size = 50;
 	creatureList[0] = new Creature();
 	creatureList[0].createAt("thisId1", 0, rand(maxWidth - size),
